21685a12f3add09560cc0735837c05577e2679ec
liangfutang <tanglf@kalman-navigation.com>
2025-04-10 17:28:19 +0800

    1. 添加序列化及接口返回

--- edu_platform/user/urls.py
+++ edu_platform/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import BookView
 
 urlpatterns = [
     path('test/', views.orm_select),
+    path('book/', BookView.as_view(), name='book'),
 ]
--- edu_platform/user/views.py
+++ edu_platform/user/views.py
@@ -1,9 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+
 from mapper import views
+from mapper.models import Book
+from rest_framework import generics
+
+from mapper.serializers import BookSerializer
+
 
 @csrf_exempt
 def orm_select(request):
     result = views.index()
     print(result)
     return HttpResponse("ok")
+
+class BookView(APIView):
+    def get(self, request, *args, **kwargs):
+        queryset = Book.objects.all()
+
+        serializer = BookSerializer(queryset, many=True)
+        return JsonResponse({
+            "status": 200,
+            "data": serializer.data
+        }, safe=False)
